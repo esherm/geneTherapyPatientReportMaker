874f73200e0001cf4750a5950b449f363dc417db
ericsherman <esherm@mail.med.upenn.edu>
2015-05-14 16:16:16 -0400

    only get GTSP metadata for samples we care about - fully resolves issue #10

--- specimen_management.R
+++ specimen_management.R
@@ -4,14 +4,20 @@
 #' @return df with columns: Trial, GTSP, Patient, Timepoint, CellType, FragMethod, VCN.
 #' @note all GTSP in th vector should be unique
 get_metadata_for_GTSP <- function(GTSP) {
-    #TODO: should repeated GTSP be allowed?
     stopifnot(length(GTSP) == length(unique(GTSP)))
-    # TODO: get only what is needed from the DB
+    GTSP = paste0("^", GTSP, "$", collapse="|")
+    
     GTSPDBconn <- dbConnect(MySQL(), group="specimen_management")
-    sets <- dbGetQuery(GTSPDBconn, "SELECT Trial, SpecimenAccNum, Patient, Timepoint, CellType, 
-                                          SamplePrepMethod, VCN
-                                   FROM gtsp;")
-    names(sets) <- c("Trial", "GTSP", "Patient", "Timepoint", "CellType", "FragMethod", "VCN")
+    
+    query = paste0("SELECT Trial, SpecimenAccNum, Patient, Timepoint, CellType,
+                           SamplePrepMethod, VCN
+                   FROM gtsp
+                   WHERE SpecimenAccNum
+                   REGEXP ", dbQuoteString(GTSPDBconn, GTSP), ";")
+    
+    sets <- dbGetQuery(GTSPDBconn, query)
     dbDisconnect(GTSPDBconn)
-    sets <- sets[ sets$GTSP %in% GTSP, ]
+    
+    names(sets) <- c("Trial", "GTSP", "Patient", "Timepoint", "CellType", "FragMethod", "VCN")
+    sets    
 }
