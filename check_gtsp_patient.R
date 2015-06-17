58c3ccc0d335abf96b7eaad97074f8db4fb02bc7
yhwu <ywu9312@gmail.com>
2015-06-17 10:26:38 -0400

    check_gtsp_patient.R: check what GTSP folders we have in terms of trial and patient

---
+++ check_gtsp_patient.R
@@ -0,0 +1,26 @@
+library("methods", quietly=TRUE)
+library("RMySQL", quietly = TRUE) #also loads DBI
+
+junk <- sapply(dbListConnections(MySQL()), dbDisconnect)
+dbConn <- dbConnect(MySQL(), group="intSitesDev237")
+
+sql <- "select trial, patient, SpecimenAccNum from specimen_management.gtsp"
+##message(sql)
+trial_pat_gtsp <- dbGetQuery(dbConn,sql)
+names(trial_pat_gtsp) <- tolower(names(trial_pat_gtsp))
+
+workDir <- commandArgs(trailingOnly=TRUE)[1]
+if( is.na(workDir) ) workDir <- "."
+workDir <- normalizePath(workDir, mustWork=TRUE)
+gtspDir <- grep("^GTSP", list.dirs(path=workDir, full.names=FALSE), ignore.case=TRUE, value=TRUE)
+
+gtsp <- sub("-\\d+$", "", gtspDir)
+
+df <- data.frame(GTSP=gtsp, sampleName=gtspDir)
+
+merged.tab <- merge(df, trial_pat_gtsp,
+                    by.x="GTSP", by.y="specimenaccnum")
+write.table(merged.tab, "", sep = "\t", row.names=FALSE, quote=FALSE)
+
+q()
+
