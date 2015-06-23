31465882137522fc71ad988893e25d55e52e2141
yhwu <ywu9312@gmail.com>
2015-06-23 14:30:02 -0400

    Looks good

--- makeGeneTherapyPatientReport.R
+++ makeGeneTherapyPatientReport.R
@@ -226,7 +226,7 @@ popSummaryTable <- popSummaryTable[,cols]
 
 timepointPopulationInfo <- melt(timepointPopulationInfo, "group")
 
-save.image("debug.RData")
+##save.image("debug.RData")
 ##end setting variables for markdown report
 
 #### begin generating markdown ####
